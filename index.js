f4b2e1e1c91c2b0e306360f922c3cd5220aaf287
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2023-10-13 06:14:06 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+// writeCards function
+function writeCards(namesArray, event) {
+    const thankYouMessages = [];
+    for (const name of namesArray) {
+      thankYouMessages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  // countDown function
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Example usage:
+  const names = ['Alice', 'Bob', 'Charlie'];
+  const event = 'birthday';
+  
+  // Calling writeCards and logging the result
+  const messages = writeCards(names, event);
+  console.log(messages);
+  
+  // Calling countDown and logging the countdown
+  countDown(5);
+  
